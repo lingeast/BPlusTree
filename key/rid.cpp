feeb2ec23b2599cd3a9dde58fda7c06973941831
Yidong <yidongl1@uci.edu>
2014-02-28 12:32:54 -0800

    Add test rid type

---
+++ key/rid.cpp
@@ -0,0 +1,32 @@
+/*
+ * rid.cpp
+ *
+ *  Created on: Feb 28, 2014
+ *      Author: root
+ */
+
+#include "rid.h"
+#include <stdlib.h>
+
+namespace BPlusTree {
+
+rid::rid() {
+	// TODO Auto-generated constructor stub
+	id = 0;
+}
+
+rid::~rid() {
+	// TODO Auto-generated destructor stub
+}
+
+void rid::load(const void* data) {
+	memcpy(&id, data, sizeof(id));
+}
+
+std::string rid::to_string() const {
+	char buffer [33];
+	sprintf(buffer,"%d",id);
+	return std::string(buffer);
+}
+
+} /* namespace BPlusTree */
---
+++ key/rid.h
@@ -0,0 +1,34 @@
+/*
+ * rid.h
+ *
+ *  Created on: Feb 28, 2014
+ *      Author: root
+ */
+
+#ifndef RID_H_
+#define RID_H_
+
+#include "serializable.h"
+
+namespace BPlusTree {
+
+class rid: public BPlusTree::serializable {
+private:
+	uint32_t id;
+public:
+	size_t length(){return sizeof(id);}
+	void* data() const {return (void *)&id;}
+
+	// load from memory, serialzable_ptr->load(ptr_to_position);
+	void load(const void *) = 0;
+
+
+	// Used for debug
+	std::string to_string() const = 0;
+	rid();
+	~rid();
+};
+
+} /* namespace BPlusTree */
+
+#endif /* RID_H_ */
