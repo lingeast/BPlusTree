2615509a5a95684ff51a9474405b7e8371b310a8
Yidong <yidongl1@uci.edu>
2014-03-04 12:17:06 -0800

    Add scan_iterator skeleton

---
+++ bptscanitr.cpp
@@ -0,0 +1,21 @@
+/*
+ * bptscanitr.cpp
+ *
+ *  Created on: Mar 4, 2014
+ *      Author: root
+ */
+
+#include "bptscanitr.h"
+
+namespace BPlusTree {
+
+bpt_scan_itr::bpt_scan_itr() {
+	// TODO Auto-generated constructor stub
+
+}
+
+bpt_scan_itr::~bpt_scan_itr() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace BPlusTree */
---
+++ bptscanitr.h
@@ -0,0 +1,21 @@
+/*
+ * bptscanitr.h
+ *
+ *  Created on: Mar 4, 2014
+ *      Author: root
+ */
+
+#ifndef BPTSCANITR_H_
+#define BPTSCANITR_H_
+
+namespace BPlusTree {
+
+class bpt_scan_itr {
+public:
+	bpt_scan_itr();
+	virtual ~bpt_scan_itr();
+};
+
+} /* namespace BPlusTree */
+
+#endif /* BPTSCANITR_H_ */
